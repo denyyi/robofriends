4975bdfe96a59be98eff5f42816a17534afe74e6
Denyi <65618271+denyyi@users.noreply.github.com>
2020-07-08 18:06:46 +0100

    Add files via upload
    INITIAL COMMIT!!

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+
+
+const state = {
+	robots: robots,
+	searchfield: ''
+
+}
+
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchfield:''
+		}
+	}
+
+	onSearchChange = (event) => {
+			this.setState({searchfield: event.target.value})
+	}
+
+
+	render () {
+		const filteredRobot = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+			<div className='tc'>
+				<h1>Robofriends</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList robots={ filteredRobot }/>
+			</div>
+		);
+	}
+
+}
+
+export default App
---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots = [
+
+{
+id:1,
+name: 'Jojo Aderinto',
+username: 'jojo',
+email: 'jojo@jojo.com',
+},
+
+{
+id:2,
+name: 'Mo Aderinto',
+username: 'Mo',
+email: 'mo@mo.com',
+},
+
+{
+id:3,
+name: 'B Aderinto',
+username: 'Buki',
+email: 'buki@buki.com',
+},
+
+{
+id:4,
+name: 'Romola Ogundairo',
+username: 'Romzy',
+email: 'romola@gmail.com',
+},
+
+{
+id:5,
+name: 'Milola Ogundairo',
+username: 'Milola',
+email: 'Milola@milola.com',
+},
+
+{
+id:6,
+name: 'Casper Aderinto',
+username: 'casper',
+email: 'casper@jojo.com',
+},
+
+{
+id:7,
+name: 'Roger Ramjet',
+username: 'roger',
+email: 'roger@mo.com',
+},
+
+{
+id:8,
+name: 'Danger Mouse',
+username: 'DM',
+email: 'dm@mo.com',
+},
+]
