b8f058f5569b7587db6ca3eee845f896b43f0b29
Denyyi <blackbugzy007@gmail.com>
2020-07-09 11:29:30 +0100

    Updates

---
+++ precache-manifest.325962b808dcb79b0afd92de01541278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c256c20d015a7bf6da78f93b16e0f83",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5560af6b8b3d516bccde",
+    "url": "/robofriends/static/css/main.360a3eb3.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5560af6b8b3d516bccde",
+    "url": "/robofriends/static/js/main.80ab1acd.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
